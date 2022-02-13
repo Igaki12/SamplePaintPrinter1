a0498da4f44eaa3d12c6e9af3093f2066ad174ae
unknown <igatatsu1997@gmail.com>
2022-02-13 19:09:18 +0900

    fixed a little

--- sample.js
+++ sample.js
@@ -123,7 +123,7 @@ ctx.strokeText("Hello World!!", 15, 65);
 ctx.lineWidth = 2;
 ctx.strokeStyle = "yellow";
 ctx.strokeText("Hello World!!", 15, 65);
-console.log(Math.ceil(ctx.measureText("Hello World!").width) + 1);
+console.log(ctx.measureText("Hello World!"));
 cans[0].style.width = Math.ceil(ctx.measureText("Hello World!").width) + 1 + "px";
 
 
